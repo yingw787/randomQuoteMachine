f99708fe8ea9dee1df8acf29b65c3d58d72e5a98
Ying Wang <yingw787@gmail.com>
2016-07-28 16:28:39 -0400

    Publishing live version for public display.

---
+++ README.md
@@ -0,0 +1,8 @@
+# randomQuoteMachine
+
+# INCLUDES WORK THAT IS NOT MY OWN.
+# functions are modified from https://bgrins.github.io/TinyColor/docs/tinycolor.html
+# HSV and golden ration configuate info from http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+# thanks also to Stack Overflow, Google, and other peeps
+
+random Quote Machine that generates quotes prettily from JSON file. 
---
+++ randomQuoteMachine.js
@@ -0,0 +1,73 @@
+var data = {};
+var quoteGlobal = "";
+var authorGlobal = "";
+
+$(document).ready(function(){
+  getJSONAndLoadQuote();
+
+  getRandomColor();
+
+});
+
+$(document).on('click', "#tweetButton", function(){
+  retweetQuote();
+})
+
+// works
+function getJSONAndLoadQuote(){
+  $.getJSON("quotes.json", function(quotes){
+    data = quotes;
+    return loadQuote();
+  });
+}
+
+// works
+function loadQuote(){
+
+  transitionColors();
+
+  var randomQuoteIndex = Math.floor(Math.random() * (data.length));
+  var randomQuote = data[randomQuoteIndex];
+
+  var quote = randomQuote.Quote;
+  var author = "-";
+  author += randomQuote.Author;
+
+  quoteGlobal = quote;
+  authorGlobal = author;
+
+  var quoteText = document.getElementById("quoteText");
+  var authorText = document.getElementById("authorText");
+
+  quoteText.innerHTML = quote;
+  authorText.innerHTML = author;
+
+  return;
+
+}
+
+function retweetQuote(){
+
+  var tweetString = "";
+  tweetString += "https://twitter.com/intent/tweet?text=";
+  tweetString += encodeURIComponent(quoteGlobal + " " + authorGlobal + " #randomQuoteMachine");
+
+  window.open(tweetString);
+}
+
+function transitionColors(){
+
+
+  var newColor = getRandomColor();
+
+  $(document.body).animate({backgroundColor: newColor}, 1000);
+  $("#authorText").animate({color: newColor}, 1000);
+  $("#quoteText").animate({color: newColor}, 1000);
+  $("#tweetButton").animate({borderTopColor: newColor, borderLeftColor: newColor, borderRightColor: newColor, borderBottomColor: newColor}, 1000);
+  $("#newQuoteButton").animate({borderTopColor: newColor, borderLeftColor: newColor, borderRightColor: newColor, borderBottomColor: newColor}, 1000);
+
+  $("#newQuoteButton").animate({color: newColor}, 1000);
+
+  // TODO: The Twitter icon still doesn't change color, don't know what to do about that
+
+}
